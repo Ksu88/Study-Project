02c6a9e81f54bc77cf6d04145e7165ee3e1b4edd
Kseniya <kseniyapopello@icloud.com>
2022-04-25 16:15:38 +0300

    change one

---
+++ script.js
@@ -0,0 +1,15 @@
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+let oneOfLastFilms = prompt('Один из последних просмотренных фильмов?', '');
+let whichRatin = prompt('На сколько оцените его?', '');
+let oneOfLastFilmsTwo = prompt('Один из последних просмотренных фильмов?', '');
+let whichRatinTwo = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[oneOfLastFilms] = whichRatin;
+personalMovieDB.movies[oneOfLastFilmsTwo] = whichRatinTwo;
+console.log(personalMovieDB);
