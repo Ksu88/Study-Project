2942dd627f895e9df963bc9e7f85664a6619f175
Kseniya <kseniyapopello@icloud.com>
2022-04-28 21:36:09 +0300

    part two

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-let oneOfLastFilms = prompt('Один из последних просмотренных фильмов?', '');
-let whichRatin = prompt('На сколько оцените его?', '');
-let oneOfLastFilmsTwo = prompt('Один из последних просмотренных фильмов?', '');
-let whichRatinTwo = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[oneOfLastFilms] = whichRatin;
-personalMovieDB.movies[oneOfLastFilmsTwo] = whichRatinTwo;
-console.log(personalMovieDB);//coment
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('error');
+    }     
+}
+if (personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+    alert('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+}
+else {
+    alert('Произошла ошибка');
+}
